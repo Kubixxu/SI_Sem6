c04eb2003970d4fc49f738da1bb4d4c67ca29c58
Jastrzab <kubixxu@gmail.com>
2022-06-13 15:43:06 +0200

    Finished data cleaning

--- main_script.py
+++ main_script.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+import numpy as np
 import pandas
 import json
 
@@ -10,6 +12,36 @@ def take_the_best_genre(ct_dict):
             curr_max = ct_dict[key]
     ct_dict.pop(curr_best)
     return curr_best
+
+def json_str_to_one_genre(x):
+    y = json.loads(x.strip())
+    curr_index = 8
+    curr_genre = ""
+    for value in y.values():
+            if value in chosen_genres and chosen_genres.index(value) < curr_index:
+                curr_genre = value
+                curr_index = chosen_genres.index(value)
+    if curr_index != 8:
+        return curr_genre
+    else:
+        return None
+
+def f1score_mathched_elems(nb_engine, chunks_x, chunks_y):
+    avg_matched = 0
+    avg_f1_score = 0.0
+    for chunk_idx in range(len(chunks_x)):
+        curr_val_chunk_x = chunks_x[chunk_idx]
+        curr_val_chunk_y = chunks_y[chunk_idx]
+        tmp_chunks_x = list(chunks_x)
+        tmp_chunks_y = list(chunks_y)
+        tmp_chunks_x.pop(chunk_idx)
+        tmp_chunks_y.pop(chunk_idx)
+        
+        curr_train_chunk_x = np.concatenate(tmp_chunks_x)
+        curr_train_chunk_y = np.concatenate(tmp_chunks_y)
+        y_pred = nb_engine.fit(curr_train_chunk_x, curr_train_chunk_y).predict(curr_val_chunk_x)
+        avg_matched += (curr_val_chunk_y == y_pred).sum()
+        avg_f1_score += f1_score(curr_val_chunk_y, y_pred)
 
 data_to_process = pandas.read_csv('../booksummaries.txt', '\t', index_col=False, header=None)
 
@@ -28,9 +60,24 @@ for column in data_to_process[[5]]:
                 count_dict[value] = 1
 
 #print(count_dict.values())
-chosen_genres = set()
+chosen_genres = []
 for i in range(7):
-    chosen_genres.add(take_the_best_genre(count_dict))
+    chosen_genres.append(take_the_best_genre(count_dict))
+
+data_to_process[5] = data_to_process[5].transform(json_str_to_one_genre)
+
+data_to_process.dropna(inplace=True)
+
+cleansed_data = data_to_process.to_numpy()
+train_n_validate_ds = cleansed_data[0:int(len(cleansed_data) * 0.9)]
+test_ds = cleansed_data[int(len(cleansed_data) * 0.9):]
+train_n_validate_ds_x = train_n_validate_ds[:,1]
+train_n_validate_ds_y = train_n_validate_ds[:,0]
+test_ds_x = test_ds[:,1]
+test_ds_y = test_ds[:,0]
+
+train_n_validate_ds_x_chunks = np.array_split(train_n_validate_ds_x, 10)
+train_n_validate_ds_y_chunks = np.array_split(train_n_validate_ds_y, 10)
 
 
-print(chosen_genres)
+f1score_mathched_elems(None, train_n_validate_ds_x_chunks, train_n_validate_ds_y_chunks)
